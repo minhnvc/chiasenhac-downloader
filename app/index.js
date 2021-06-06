6edd48be3dc216914637145e7386edf73dc5760d
minhnvc <minhnvc@vng.com.vn>
2021-06-06 23:59:56 +0700

    fix bug arg index

--- app/index.js
+++ app/index.js
@@ -2,15 +2,19 @@
 const args = process.argv.slice(2)
 const Album = require('./csn/album')
 const Terminal = require('./utils/console')
-const terminal = new Terminal()
 
 if (args[0] === 'album') {
   const email = args[1]
-  const password = args[3]
-  const albumUrl = args[4]
-  const album = new Album(email, password, albumUrl)
-  album.start()
-  terminal.addWatcher(album.processDownload)
+  const password = args[2]
+  const albumUrl = args[3]
+  if (!email || !password || !albumUrl) {
+    console.log('Please try again: chiasenhac album {email} {password} {album-link}')
+  } else {
+    const album = new Album(email, password, albumUrl)
+    album.start()
+    const terminal = new Terminal()
+    terminal.addWatcher(album.processDownload)
+  }
 } else {
   console.error('Invalid type')
 }
